7eebac96921131e97e04cf8e0488edc1f97a277b
ricciliao <riccix@163.com>
2025-06-09 17:57:29 +0800

    feat: remove autoconfiguration of MdcSupportFilter.class.

--- src/main/java/ricciliao/message/configuration/MessageBeanConfig.java
+++ src/main/java/ricciliao/message/configuration/MessageBeanConfig.java
@@ -5,18 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import ricciliao.message.filter.MessageFilter;
+import ricciliao.x.log.MdcSupportFilter;
 
 @Configuration
 public class MessageBeanConfig {
 
     @Bean
-    public FilterRegistrationBean<MessageFilter> httpServletWrapperFilter() {
-
+    public FilterRegistrationBean<MessageFilter> messageFilter() {
         FilterRegistrationBean<MessageFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new MessageFilter());
         registrationBean.addUrlPatterns("/*");
-        registrationBean.setOrder(Ordered.LOWEST_PRECEDENCE - 1);
-        registrationBean.setName("httpServletWrapperFilter");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+        registrationBean.setName("messageFilter");
+
+        return registrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<MdcSupportFilter> mdcSupportFilter() {
+        FilterRegistrationBean<MdcSupportFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new MdcSupportFilter());
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        registrationBean.setName("mdcSupportFilter");
 
         return registrationBean;
     }
